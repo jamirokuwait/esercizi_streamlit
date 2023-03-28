3ac4606b592d9f17d2e380997b78e319546bdddc
jamirokuwait <43453067+jamirokuwait@users.noreply.github.com>
2023-03-28 14:36:07 +0200

    fixed es 3

--- Es3/app.py
+++ Es3/app.py
@@ -2,10 +2,34 @@ import streamlit as st
 import pandas as pd
 
 def main():
+    
+    def paridispari (a):
+        if a % 2 == 0:
+            pari = 'il numero è pari'
+            return pari
+        else:
+            dispari = 'il numero è dispari'
+            return dispari
+    
+    
     st.title('es. numero 3')
     
+    num1 = st.slider('inserisci un numero!',1,100)
     
+    caso1 = paridispari(num1)
     
+    st.write(caso1)
+    
+    num2 = st.slider('inserisci un numero!',1,100)
+    
+    caso2 = paridispari(num2)
+    
+    st.write(caso2)
     
     if __name__ == '__main__':
         main()
+        
+        
+        
+        #Es3) Inserendo un numero intero determinare se è pari o dispari 
+        # (anche qui input come slicer ed input come cella testo)
