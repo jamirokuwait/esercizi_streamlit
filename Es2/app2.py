6143af332ef96372eb9361b920dd598cc5b98b73
jamirokuwait <43453067+jamirokuwait@users.noreply.github.com>
2023-03-28 14:17:43 +0200

    es 2 fixed

--- Es2/app2.py
+++ Es2/app2.py
@@ -1,14 +1,30 @@
 import streamlit as st
 import pandas as pd
 
-# def main():
-st.title('Esercizio numero 2')
+def main():
+    st.title('Esercizio numero 2')
 
-num1 = st.slider('inserire un numero',0,100)
-st.write("I'm ", num1, 'years old')
-num2 = st.slider('inserire un altro numero',0,100)
-num3 = st.slider('inserire un terzo numero',0,100)
+    num1 = st.slider('inserire il primo numero',0,100)
+    st.write("ho ", num1, ' anni')
+    num2 = st.slider('inserire il secondo numero',0,100)
+    st.write("ho ", num2, ' anni')
+    num3 = st.slider('inserire il terzo numero',0,100)
+    st.write("ho ", num3, ' anni')
 
+    def checknum (a,b,c):
+        if a > b and a > c:
+            caso = st.write(a,'è il numero maggiore dei tre')
+            return caso
+        elif b > a and b > c:
+            caso = st.write(b,'è il numero maggiore dei tre')
+            return caso
+        elif c > a and c > b:
+            caso = st.write(c,'è il numero maggiore dei tre')
+            return caso
+        
+    caso = checknum(num1,num2,num3)
 
-# if __name__ == '__main__':
-#     main()
+    st.write(caso)
+    
+if __name__ == '__main__':
+    main()
