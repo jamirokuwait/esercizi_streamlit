62221362d4f6bdfcc2dc34cda971a17e5013f99d
jamirokuwait <43453067+jamirokuwait@users.noreply.github.com>
2023-03-28 13:30:29 +0200

    giao

--- Es2/app2.py
+++ Es2/app2.py
@@ -5,8 +5,8 @@ import pandas as pd
 st.title('Esercizio numero 2')
 
 num1 = st.slider('inserire un numero',0,100,1)
-num2 = st.slider()
-num3 = st.slider()
+num2 = st.slider('inserire un numero',0,100,1)
+num3 = st.slider('inserire un numero',0,100,1)
 
 
 # if __name__ == '__main__':
