5f32f78a23345b77e5f488f71e147a4aa2fc35ba
jamirokuwait <43453067+jamirokuwait@users.noreply.github.com>
2023-03-28 13:12:22 +0200

    ciao

--- Es 2/app.py
+++ Es 2/app.py
@@ -2,9 +2,11 @@ import streamlit as st
 import pandas as pd
 
 def main():
-    st.title('es. numero 2')
+    st.title('Esercizio numero 2')
     
-    
+    num1 = st.slider('inserire un numero',0,100,1)
+    num2 = st.slider()
+    num3 = st.slider()
     
     
     if __name__ == '__main__':
