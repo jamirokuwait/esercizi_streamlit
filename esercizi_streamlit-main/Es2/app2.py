5c777816fe5c65d5032b216500cf569971e72a6d
jamirokuwait <43453067+jamirokuwait@users.noreply.github.com>
2023-03-29 10:01:53 +0200

    fix

--- esercizi_streamlit-main/Es2/app2.py
+++ esercizi_streamlit-main/Es2/app2.py
@@ -28,18 +28,25 @@ def main():
     st.title('Esercizio numero 2')
 
     num1 = st.slider('inserire il primo numero', 0, 100)
+    num1_1 = st.number_input('inserisci un numero!',0,1000)
     st.write("ho ", num1, ' anni')
+    st.write("ho ", num1_1, ' anni','(numb input)')
 
     num2 = st.slider('inserire il secondo numero', 0, 100)
+    num2_1 = st.number_input('inserisci un numero!',0,1000)
     st.write("ho ", num2, ' anni')
+    st.write("ho ", num2_1, ' anni','(numb input)')
 
     num3 = st.slider('inserire il terzo numero', 0, 100)
+    num3_1 = st.number_input('inserisci un numero!',0,1000)
     st.write("ho ", num3, ' anni')
+    st.write("ho ", num3_1, ' anni','(numb input)')
 
     caso = checknummax(num1, num2, num3),checknummin(num1,num2,num3)
+    caso2 = checknummax(num1_1, num2_1, num3_1),checknummin(num1_1,num2_1,num3_1)
     
     st.write(caso)
-
+    st.write(caso2)
 
 if __name__ == '__main__':
     main()
