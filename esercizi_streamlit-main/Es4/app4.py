d4d20f75ccc5cbd7e0ccbe6aa19975435468a9bb
jamirokuwait <43453067+jamirokuwait@users.noreply.github.com>
2023-04-06 12:32:24 +0200

    miao

--- esercizi_streamlit-main/Es4/app4.py
+++ esercizi_streamlit-main/Es4/app4.py
@@ -2,8 +2,13 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('energia.csv')
-df
+# df = pd.read_csv('energia.csv')
+# df
+
+Energia = st.file_uploader("upload file", type={"csv", "txt"})
+if Energia is not None:
+    Energia_df = pd.read_csv(Energia)
+st.write(Energia_df)
 # def main():
 # if __name__ == '__main__':
 #     main()
