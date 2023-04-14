35e9532b627589e378ececa0e218bdbabf88a0f5
jamirokuwait <43453067+jamirokuwait@users.noreply.github.com>
2023-04-14 20:04:04 +0200

    Add files via upload

--- esercizi_streamlit-main/Es4/appBologna.py
+++ esercizi_streamlit-main/Es4/appBologna.py
@@ -11,19 +11,15 @@ import streamlit as st
 from FCPython import createPitch
 
 
-
- 
-
 pitch_width = 120
 pitch_height = 80
 fig, ax = createPitch(pitch_width, pitch_height, 'yards', 'gray')
 
-fig
 
 # st.set_page_config(layout="wide")
 
 st.title('Bologna Calcio 2021-2022')
-st.subheader('Players by G+A')
+
 df = pd.read_csv('Es4/Bologna_22.csv')
 df1 = df.sort_values(by=['G+A'], ascending=False)
 df2 = df.sort_values(by=['Nation'])
@@ -31,13 +27,15 @@ df2 = df2.dropna()
 df3 = df.sort_values(by=['Min'], ascending=False)
 df3 = df3.dropna()
 df1 = df1.dropna()
-df1
-st.subheader('Players by Nation')
-df2
-st.write('Bologna players by nationality(players with at least 1 min)')
-st.subheader('Players by Min')
-df3
-st.write('Bologna players by min played(players with at least 1 min)')
+
+st.subheader('Bologna Calcio players stat for 21/22 season')
+tab1, tab2, tab3 = st.tabs(['Goal + Assist', 'Nation', 'Mins played'])
+with tab1:
+    df1
+with tab2:
+    df2
+with tab3:
+    df3
 
 
 # dfplayer = df['Player']
@@ -54,43 +52,46 @@ st.write('Bologna players by min played(players with at least 1 min)')
 
 # SETTING THE TITLE
 title = "Player selector"  # SETTING LAYOUT
-#
-# using whole page#DISPLAYING THE TITLE
 st.title(title)
-# DISPLAYING A HORIZONTAL LINE TO SEPERATE CONTENT
-st.markdown("---")
-# CREATING A SIDEBAR WITH FILTER FOR PLAYERS
-st.header("Choose a player:")
-player = st.selectbox(
-    'Select here:',
-    options=df["Player"].unique())  # posso scegliere come opzione ogni valore univoco sulla colonna players
+with st.expander('Click for display the player row and his stats'):
+    # using whole page#DISPLAYING THE TITLE
 
-df_selection = df.query(  # seleziona tutta la riga
-    # @ si riferisce ad una variabile creata da me,gli sto dicendo di assegnare a df_selections la riga dove avviene la query,che entry ho scelto sulla colonna Players
-    "Player == @player"
-)
-st.subheader('Full player stats')
-df_selection
-# SELECTING THE STATS FROM THE DATA THAT WILL BE DISPLAYED
+    # DISPLAYING A HORIZONTAL LINE TO SEPERATE CONTENT
+    st.markdown("---")
+    # CREATING A SIDEBAR WITH FILTER FOR PLAYERS
+    st.header("Choose a player:")
+    player = st.selectbox(
+        'Select here:',
+        options=df["Player"].unique())  # posso scegliere come opzione ogni valore univoco sulla colonna players
 
-# seleziono gls dalla riga corrispondente del giocatore
-goals = df_selection["Gls"].sum()
-assists = df_selection["Ast"].sum()
-expectedgoals = df_selection["xG"].sum()
-# DISPLAYING THE SELECTED STATS INSIDE COLUMNS
-left_column, middle_column, right_column, rightright_column = st.columns(4)
-with left_column:
-    st.subheader("Goals")
-    st.subheader(f"{goals}")
-with middle_column:
-    st.subheader("Assists")
-    st.subheader(f"{assists}")
-with right_column:
-    st.subheader("Expected goals")
-    st.subheader(f"{expectedgoals}")
-with rightright_column:
-    st.subheader("Player Name")
-    st.subheader(f"{player}")
+    df_selection = df.query(  # seleziona tutta la riga
+        # @ si riferisce ad una variabile creata da me,gli sto dicendo di assegnare a df_selections la riga dove avviene la query,che entry ho scelto sulla colonna Players
+        "Player == @player"
+    )
+    st.subheader('Full player stats')
+    df_selection
+    # SELECTING THE STATS FROM THE DATA THAT WILL BE DISPLAYED
 
+    # seleziono gls dalla riga corrispondente del giocatore
+    goals = df_selection["Gls"].sum()
+    assists = df_selection["Ast"].sum()
+    expectedgoals = df_selection["xG"].sum()
+    # DISPLAYING THE SELECTED STATS INSIDE COLUMNS
+    left_column, middle_column, right_column, rightright_column = st.columns(4)
+    with left_column:
+        st.subheader("Goals")
+        st.subheader(f"{goals}")
+    with middle_column:
+        st.subheader("Assists")
+        st.subheader(f"{assists}")
+    with right_column:
+        st.subheader("Expected goals")
+        st.subheader(f"{expectedgoals}")
+    with rightright_column:
+        st.subheader("Player Name")
+        st.subheader(f"{player}")
+
+
+# fig
 
 # set up the figures
